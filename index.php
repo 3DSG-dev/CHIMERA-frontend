d63ea47f5e2dded99d7ecfc9046009e46dd1f7c9
Francesca Lori <WEB287\lori>
2019-02-01 13:00:56 +0000

    refactor nomi delle classi e sistemazione codice auth.php con dbconn chiusa correttamente e js con scope corretti.
    git-tfs-id: [https://tfs.bim3dsg.polimi.it/PoliBIM/]$/BIM3DSG;C242

--- index.php
+++ index.php
@@ -23,79 +23,85 @@
         <link rel="stylesheet" type="text/css" href="css/main.css"/>
     </head>
     <body>
-    <div id="pageContainer">
-        <div id="headerContainer">
-            <div id="headerLoghi">
+    <div id="page-container">
+        <div id="header-container">
+            <div id="header-loghi">
                 <div id="loghi-3dsurvey">
-                    <div id="logo3DSurveySmall" class="loghi">
-                        <a  id="logo3DSurveyLink" href="http://www.sitech-3dsurvey.polimi.it" target="_blank">
+                    <div id="logo-3dsurvey-small" class="loghi">
+                        <a class="logo-3dsurvey-link" href="http://www.sitech-3dsurvey.polimi.it" target="_blank">
                             <img src="img/logo-3dsurvey-60.png">
                         </a>
                     </div>
-                    <div id="logo3DSurveyLarge" class="loghi">
-                        <a  id="logo3DSurveyLink" href="http://www.sitech-3dsurvey.polimi.it" target="_blank">
+                    <div id="logo-3dsurvey-large" class="loghi">
+                        <a class="logo-3dsurvey-link" href="http://www.sitech-3dsurvey.polimi.it" target="_blank">
                             <img src="img/logo-3dsurvey-full-60.png">
                         </a>
                     </div>
                 </div>
                 <div id="loghi-polimi">
-                    <div id="logoPolimiSmall" class="loghi">
-                        <a  id="logoPolimiLink" href="http://www.polimi.it" target="_blank">
+                    <div id="logo-polimi-small" class="loghi">
+                        <a class="logo-polimi-link" href="http://www.polimi.it" target="_blank">
                             <img src="img/logo-polimi-60.png">
                         </a>
                     </div>
 
-                    <div id="logoPolimiLarge" class="loghi">
-                        <a  id="logoPolimiLink" href="http://www.polimi.it" target="_blank">
+                    <div id="logo-polimi-large" class="loghi">
+                        <a class="logo-polimi-link" href="http://www.polimi.it" target="_blank">
                             <img src="img/logo-polimi-testo-60.png">
                         </a>
                     </div>
                 </div>
+
                 <?php
-                    if (isset($_SESSION['validUser'])) {
-                        echo '
-                            <div class="logout-container">
+                if (isset($_SESSION['validUser'])) {
+                    echo '
+                            <div id="logout-container">
                                 <div class="logout-picture image">
-                                    <a href="php/logout.php" title="Disconnetti ' . $_SESSION['validUserName'] . '" data-ajax="false"><img src="img/lock-icon-grey.png"></a>
+                                    <a href="php/logout.php" title="Disconnetti ' . $_SESSION['validUserName'] . '"><img src="img/lock-icon-grey.png"></a>
                                 </div>
                             </div>
-                            <div class="user-container">
+                            <div id="user-container">
                                 <div class="user-picture image" style=\'background-image: url("img/user-icon.png")\'></div>
-                                <div id="actualUser" class="user-label">' . $_SESSION['FullName'] . '</div>
+                                <div id="actual-user" class="user-label">' . $_SESSION['FullName'] . '</div>
                             </div>
                         ';
-                    }
+                }
                 ?>
+
             </div>
         </div>
 
         <?php if (!isset($_SESSION['validUser']))
             {
                 echo '
-                    <div id="loginDialog"></div>
+                    <div id="login-dialog"></div>
                 ';
             }
         ?>
 
-        <div class="mainareaContainer">
-            <div class="pageTitleContainer">
-                <div class="pageTitle">QUICK TEXT VIEWER</div>
-                <div class="pageSubtitle">Choose, through the drop-down menus, the object for which you want to enter information and click on the button "SEARCH"</div>
+        <div id="mainarea-container">
+            <div id="page-title-container">
+                <div id="page-title">TESTUAL MODE</div>
+                <div id="page-subtitle">Fill in the boxes and click on the search button.</div>
             </div>
-            <div class="pageContentContainer">
-                <div class="pageContent"></div>
+            <div id="page-section">
+                <div id="page-row">
+                    <div id="page-column">
+
+                    </div>
+                </div>
             </div>
         </div>
 
         <div id="side-toolbar-container">
             <div id="side-toolbar-list"  class="k-widget k-listview k-selectable">
                 <div class="side-tool-item" >
-                    <span id="openInfoWindowBtn" class="side-tool-btn" title="Infos">
+                    <span id="open-info-window-btn" class="side-tool-btn" title="Info board">
                         <img src="img/info-icon.png" class="side-tool-btn-image">
                     </span>
                 </div>
                 <div class="side-tool-item">
-                     <span id="resetViewBtn" class="side-tool-btn" title="Reset view">
+                     <span id="reset-view-btn" class="side-tool-btn" title="Reset view">
                         <img src="img/refresh-icon.png" class="side-tool-btn-image">
                      </span>
                 </div>
