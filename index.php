aeb6f4b709072e77d0ea6c9ecd2938672773c1e1
Francesca Lori <WEB287\lori>
2019-02-05 13:24:15 +0000

    Disposizione elementi di pagina - prima versione dei testi - modifica pulsanti in toolbar
    git-tfs-id: [https://tfs.bim3dsg.polimi.it/PoliBIM/]$/BIM3DSG;C243

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
         <meta charset="iso-8859-15">
 
         <!-- KENDO UI -->
+
+        <!--<link href="libs/KendoUI/styles/kendo.common.min.css" rel="stylesheet" /> <!-- Common Kendo UI CSS for web widgets and widgets for data visualization. -->
         <link href="libs/KendoUI/styles/kendo.rtl.min.css" rel="stylesheet" type="text/css" /><!-- (Optional) RTL CSS for Kendo UI widgets for the web. Include only in right-to-left applications. -->
         <link href="libs/KendoUI/styles/kendo.default.min.css" rel="stylesheet" type="text/css" /><!-- Default Kendo UI theme CSS for web widgets and widgets for data visualization. -->
         <link href="libs/KendoUI/styles/kendo.default.mobile.min.css" rel="stylesheet" type="text/css" /><!-- (Optional) Kendo UI Hybrid CSS. Include only if you will use the mobile devices features. -->
@@ -80,14 +82,182 @@
         ?>
 
         <div id="mainarea-container">
-            <div id="page-title-container">
-                <div id="page-title">TESTUAL MODE</div>
-                <div id="page-subtitle">Fill in the boxes and click on the search button.</div>
-            </div>
-            <div id="page-section">
-                <div id="page-row">
-                    <div id="page-column">
+            <div class="page-section search-form-section">
 
+                <div class="page-row page-title-row">
+                    <div class="page-column page-title-column">
+                        <h1 class="page-title">TEXTUAL MODE</h1>
+                    </div>
+                </div>
+
+                <div class="page-row search-form-row">
+                    <div class="page-column search-title-column">
+                        <h2 class="search-title">View Saqqara site items as a list</h2>
+                        <!--<h2 class="search-title">Work with Saqqara site items</h2>-->
+                        <p class="search-subtitle">View all the items you want to work on by filtering them with the <b>Search fields</b> tab or by clicking the <b>Use your list</b> tab.</p>
+                        <p class="notes">NOTES: - Your list can only be modified via 3D mode. - Search fields works even if you leave some fields empty.</p>
+                    </div>
+                    <div class="page-column search-form-column">
+                        <div class="search-form-tabstrip">
+                            <ul>
+                                <li class="k-state-active">Search fields</li>
+                                <li>Use your list</li>
+                            </ul>
+                            <div class="searchform-tabitem">
+                                <div class="inputSelectObject">
+                                    <label class="inputLayerLabel">Layer0</label>
+                                    <input id="inputLayer0" type="text" class="k-textbox inputLayer"/>
+                                </div>
+                                <div class="inputSelectObject">
+                                    <label class="inputLayerLabel">Layer1</label>
+                                    <input id="inputLayer1" type="text" class="k-textbox inputLayer">
+                                </div>
+                                <div class="inputSelectObject">
+                                    <label class="inputLayerLabel">Layer2</label>
+                                    <input id="inputLayer2" type="text" class="k-textbox inputLayer">
+                                </div>
+                                <div class="inputSelectObject">
+                                    <label class="inputLayerLabel">Layer3</label>
+                                    <input id="inputLayer3" type="text" class="k-textbox inputLayer">
+                                </div>
+                                <div class="inputSelectObject">
+                                    <label class="inputLayerLabel">Name</label>
+                                    <input id="inputName" type="text" class="k-textbox inputLayer">
+                                </div>
+                                <div class="inputSelectObject">
+                                    <label class="inputLayerLabel">Version</label>
+                                    <input id="inputVersion" type="text" class="k-textbox inputLayer">
+                                </div>
+                                <div style="clear:both;"></div>
+                                <div class="btnSelectObjectWrap">
+                                    <button class="k-button k-primary btnSelectObject">Search</button>&nbsp;
+                                </div>
+                            </div>
+                            <div class="searchform-tabitem">
+                                <div class="btnSelectObjectWrap">
+                                    <button class="k-button k-primary btnSelectObject">Use you list</button>&nbsp;
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                            <style>
+
+                                .search-form-tabstrip h2 {
+                                    font-weight: lighter;
+                                    font-size: 5em;
+                                    line-height: 1;
+                                    padding: 0 0 0 30px;
+                                    margin: 0;
+                                }
+
+                                .search-form-tabstrip h2 span {
+                                    background: none;
+                                    padding-left: 5px;
+                                    font-size: .3em;
+                                    vertical-align: top;
+                                }
+
+                                .search-form-tabstrip p {
+                                    margin: 0;
+                                    padding: 0;
+                                }
+                            </style>
+
+                            <script>
+                                $(document).ready(function() {
+                                    $(".search-form-tabstrip").kendoTabStrip({
+                                        animation:  {
+                                            open: {
+                                                effects: "fadeIn"
+                                            }
+                                        }
+                                    });
+                                });
+                            </script>
+
+                    <!----
+                                        <div class="page-column form-column">
+                                            <div class="inputSelectObject">
+                                                <label for="inputLayer0">Layer 0</label>
+                                                <input id="inputLayer0" type="text" class="k-textbox inputLayer"/>
+                                            </div>
+                                            <div class="inputSelectObject">
+                                                <label for="inputLayer1">Layer 1</label>
+                                                <input id="inputLayer1" type="text" class="k-textbox inputLayer">
+                                            </div>
+                                            <div class="inputSelectObject" style="margin-right:0;">
+                                                <label for="inputLayer2">Layer 2</label>
+                                                <input id="inputLayer2" type="text" class="k-textbox inputLayer">
+                                            </div>
+                                            <div style="clear:both;"></div>
+                                            <div class="inputSelectObject">
+                                                <label for="inputLayer3">Layer 3</label>
+                                                <input id="inputLayer3" type="text" class="k-textbox inputLayer">
+                                            </div>
+                                            <div class="inputSelectObject">
+                                                <label for="inputName">Name</label>
+                                                <input id="inputName" type="text" class="k-textbox inputLayer">
+                                            </div>
+                                            <div class="inputSelectObject">
+                                                <label for="inputVersion">Version</label>
+                                                <input id="inputVersion" type="text" class="k-textbox inputLayer">
+                                            </div>
+                                            <div style="clear:both;"></div>
+                                            <div class="btnSelectObjectWrap">
+                                                <button class="k-button k-primary btnSelectObject">Ricerca</button>&nbsp;
+                                                <p style="margin-top:6px;">oppure</p>
+                                            </div>
+
+                                            <div class="btnSelectObjectWrap">
+                                                <button class="k-button k-primary btnSelectObject">Usa la tua lista</button>&nbsp;
+                                            </div>
+                                        </div>
+                                        --->
+
+
+            <div class="page-section gridResultSection">
+                <div class="page-row gridResultRow">
+                    <div class="page-column gridResultColumn">
+                        <div id="gridResult"></div>
+
+                        <script src="https://demos.telerik.com/kendo-ui/content/shared/js/products.js"></script>
+                        <script>
+                            $(document).ready(function() {
+                                $("#gridResult").kendoGrid({
+                                    dataSource: {
+                                        data: products,
+                                        schema: {
+                                            model: {
+                                                fields: {
+                                                    ProductName: { type: "string" },
+                                                    UnitPrice: { type: "number" },
+                                                    UnitsInStock: { type: "number" },
+                                                    Discontinued: { type: "boolean" }
+                                                }
+                                            }
+                                        },
+                                        pageSize: 20
+                                    },
+                                    height: 550,
+                                    scrollable: true,
+                                    sortable: true,
+                                    filterable: true,
+                                    pageable: {
+                                        input: true,
+                                        numeric: false
+                                    },
+                                    columns: [
+                                        "ProductName",
+                                        { field: "UnitPrice", title: "Unit Price", format: "{0:c}", width: "130px" },
+                                        { field: "UnitsInStock", title: "Units In Stock", width: "130px" },
+                                        { field: "Discontinued", width: "130px" }
+                                    ]
+                                });
+                            });
+                        </script>
                     </div>
                 </div>
             </div>
@@ -101,8 +271,13 @@
                     </span>
                 </div>
                 <div class="side-tool-item">
-                     <span id="reset-view-btn" class="side-tool-btn" title="Reset view">
-                        <img src="img/refresh-icon.png" class="side-tool-btn-image">
+                     <span id="reset-list-btn" class="side-tool-btn" title="Reset list">
+                        <img src="img/refresh-list-icon.png" class="side-tool-btn-image">
+                     </span>
+                </div>
+                <div class="side-tool-item">
+                     <span id="tred-mode-btn" class="side-tool-btn" title="3D mode">
+                        <img src="img/3dmode-icon.png" class="side-tool-btn-image">
                      </span>
                 </div>
             </div>
