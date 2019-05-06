0a37cdc0fede0ca4a696675229d04dc55100bf85
ITinnovations <72745273+ITinnovations@users.noreply.github.com>
2019-05-06 14:21:12 +0000

    fix & rewrite save information
    git-tfs-id: [https://tfs.bim3dsg.polimi.it/PoliBIM/]$/BIM3DSG;C424

---
+++ php/setInformationText.php
@@ -0,0 +1,27 @@
+<?php
+    include("./defaultStart.php");
+
+    $dbReference = isset($_GET['dbReference']) ? $_GET['dbReference'] : null;
+    $codiceRiferimento = isset($_GET['codiceRiferimento']) ? $_GET['codiceRiferimento'] : null;
+    $codiceCampo = isset($_GET['codiceCampo']) ? $_GET['codiceCampo'] : null;
+    $valore = isset($_GET['valore']) ? $_GET['valore'] : null;
+
+    if (!IsNullOrEmptyString($codiceRiferimento) && !IsNullOrEmptyString($codiceCampo)) {
+        if (IsNullOrEmptyString($valore)) {
+            $SQL = "SELECT set" . $dbReference . "infoschedavalue($codiceRiferimento, $codiceCampo, '')";
+        }
+        else {
+            $valore = pg_escape_literal($valore);
+            $SQL = "SELECT set" . $dbReference . "infoschedavalue($codiceRiferimento, $codiceCampo, $valore)";
+        }
+    }
+    else {
+        die("Invalid parameters");
+    }
+
+    $result = pg_query($dbConnection, $SQL) or die ("Error: $SQL");
+
+    echo json_encode("ok");
+
+    include("./defaultEnd.php");
+?>
