be52759da137e4baf6fb45765c99b44aba2126e3
ITinnovations <72745273+ITinnovations@users.noreply.github.com>
2020-03-06 00:05:25 +0000

    Possibilità di ordinamento della lista dei layer all'interno dei gruppi
    git-tfs-id: [https://tfs.bim3dsg.polimi.it/PoliBIM/]$/BIM3DSG;C493

--- php/getGisLayers.php
+++ php/getGisLayers.php
@@ -1,7 +1,7 @@
 <?php
     include("./defaultStart.php");
 
-    $SQL = 'SELECT * FROM "GisLayers" ORDER BY "Group", "Name"';
+    $SQL = 'SELECT * FROM "GisLayers" ORDER BY "Group", "OrderInsideGroup"';
 
     $result = pg_query($dbConnection, $SQL) or die ("Error: $SQL");
     $rowArray = array();
